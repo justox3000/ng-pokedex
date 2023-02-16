6b760969fe667e3617569c7ac6cbf0a6804109db
Justus Lambrecht <justuslambrecht@icloud.com>
2023-02-16 10:50:06 +0100

    created components list and view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule
